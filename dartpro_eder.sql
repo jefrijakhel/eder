d292d7c08ca45ff9b88f9f3adf00ab9537c19e5c
imaduddinharis <imaduddinhariss@gmail.com>
2019-07-02 19:50:22 +0700

    Update Database

--- dartpro_eder.sql
+++ dartpro_eder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Jun 2019 pada 12.14
+-- Waktu pembuatan: 02 Jul 2019 pada 14.49
 -- Versi server: 10.1.32-MariaDB
 -- Versi PHP: 7.2.5
 
@@ -25,6 +25,134 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `bahan_baku`
+--
+
+CREATE TABLE `bahan_baku` (
+  `id_bahan_baku` int(11) NOT NULL,
+  `nama_bahan_baku` varchar(50) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cart`
+--
+
+CREATE TABLE `cart` (
+  `id_cart` int(11) NOT NULL,
+  `meja` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `notes` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kompopsisi`
+--
+
+CREATE TABLE `kompopsisi` (
+  `id_komposisi` int(11) NOT NULL,
+  `id_bahan_baku` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `jumlah_bahan_baku` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `meja`
+--
+
+CREATE TABLE `meja` (
+  `id_meja` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `no_meja` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `nama_customer` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `no_hp` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `meja`
+--
+
+INSERT INTO `meja` (`id_meja`, `username`, `password`, `no_meja`, `status`, `nama_customer`, `email`, `no_hp`, `created_at`, `updated_at`) VALUES
+(1, 'meja1', '+meja1', '1', 0, 'duds', 'imaduddinhariss@gmail.com', '081210113977', '2019-07-01 20:40:48', '2019-07-01 20:40:48');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL,
+  `sub_menu` varchar(50) NOT NULL,
+  `nama_menu` varchar(50) NOT NULL,
+  `deskripsi_menu` text NOT NULL,
+  `jenis_menu` varchar(50) NOT NULL,
+  `harga_menu` int(11) NOT NULL,
+  `vendor` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `menu`
+--
+
+INSERT INTO `menu` (`id_menu`, `sub_menu`, `nama_menu`, `deskripsi_menu`, `jenis_menu`, `harga_menu`, `vendor`, `created_at`, `updated_at`) VALUES
+(1, 'ice coffee', 'Kopi Tubruk', 'Kopi Tubruk', 'minuman', 15000, 'elther', '2019-07-01 20:05:39', '2019-07-01 20:05:39'),
+(2, 'hot coffee', 'Bowl 1', 'Chicken Popcorn Scramble egg, Vegie', 'makanan', 25000, 'elther', '2019-07-01 20:07:43', '2019-07-01 20:07:43');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `meja` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `notes` text NOT NULL,
+  `nama_customer` varchar(50) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `no_hp` varchar(50) DEFAULT NULL,
+  `status` varchar(50) NOT NULL,
+  `estimated_time` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `meja`, `id_menu`, `qty`, `notes`, `nama_customer`, `email`, `no_hp`, `status`, `estimated_time`, `created_at`, `updated_at`) VALUES
+(3, 1, 1, 1, '', 'Duds', 'imaduddinhariss@gmail.com', '081210113977', 'diproses', '2019-07-02 11:24:26', '2019-07-02 07:19:54', '2019-07-02 11:24:26'),
+(4, 1, 2, 1, 'Notes Bowl 1', 'Duds', 'imaduddinhariss@gmail.com', '081210113977', 'diproses', '2019-07-02 11:32:40', '2019-07-02 07:19:54', '2019-07-02 11:32:40'),
+(5, 1, 2, 2, 'pesan lagi', 'duds', 'imaduddinhariss@gmail.com', '081210113977', 'ordered', NULL, '2019-07-02 10:11:52', '2019-07-02 10:11:52');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `users`
 --
 
@@ -38,8 +166,54 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`uid`, `username`, `password`, `privillege`, `created_at`, `updated_at`) VALUES
+(1, 'kasir', '+kasir', 'kasir', '2019-07-02 08:50:47', '2019-07-02 08:50:47'),
+(2, 'dapur', '+dapur', 'dapur', '2019-07-02 08:50:47', '2019-07-02 08:50:47'),
+(3, 'manager', '+manager', 'manager', '2019-07-02 08:51:07', '2019-07-02 08:51:07');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `bahan_baku`
+--
+ALTER TABLE `bahan_baku`
+  ADD PRIMARY KEY (`id_bahan_baku`);
+
+--
+-- Indeks untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id_cart`);
+
+--
+-- Indeks untuk tabel `kompopsisi`
+--
+ALTER TABLE `kompopsisi`
+  ADD PRIMARY KEY (`id_komposisi`);
+
+--
+-- Indeks untuk tabel `meja`
+--
+ALTER TABLE `meja`
+  ADD PRIMARY KEY (`id_meja`),
+  ADD UNIQUE KEY `no_meja` (`no_meja`);
+
+--
+-- Indeks untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`);
 
 --
 -- Indeks untuk tabel `users`
@@ -52,10 +226,46 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `bahan_baku`
+--
+ALTER TABLE `bahan_baku`
+  MODIFY `id_bahan_baku` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id_cart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `kompopsisi`
+--
+ALTER TABLE `kompopsisi`
+  MODIFY `id_komposisi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `meja`
+--
+ALTER TABLE `meja`
+  MODIFY `id_meja` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
